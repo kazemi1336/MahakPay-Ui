55394930241b0071e15f3718ab78a924a03ff554
persiamahak@gmail.com <persiamahak@gmail.com>
2020-11-07 00:07:42 +0330

    S03-E07-Auth-LoginAndRegister

--- Mahak-Admin/src/app/app.module.ts
+++ Mahak-Admin/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
 import { PanelModule } from './panel/panel.module';
 import { RouterModule } from '@angular/router';
 import { adminRoutes } from './routes/routes';
@@ -14,7 +14,7 @@ import { BrowserModule } from '@angular/platform-browser';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoginModule,
+    AuthModule,
     PanelModule,
     RouterModule.forRoot(adminRoutes)
   ],
